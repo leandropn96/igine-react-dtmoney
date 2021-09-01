dcac3a3bc08f00cdb9f03097e157a1e84e23aa08
leandrop <leandro.p.n96@gmail.com>
2021-08-31 23:12:51 -0300

    feat: :sparkles: Foi adicionado o modal no app e estilizado.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { TransactionsTable } from './components/TransactionsTable';
+import { NewTransactionModal } from './components/NewTransactionModal'
 import { GlobalStyle } from './styles/global';
+import Modal from 'react-modal'
+import { useState } from 'react';
+
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNerwTransactionModalOpen, setIsNerwTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNerwTransactionModalOpen(true)
+  }
+
+  function handleOpenCloseTransactionModal() {
+    setIsNerwTransactionModalOpen(false)
+  }
   return (
     <>
-      <Header></Header>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}></Header>
       <Dashboard></Dashboard>
+      <NewTransactionModal
+        isOpen={isNerwTransactionModalOpen}
+        onRequestClose={handleOpenCloseTransactionModal} />
+
       <TransactionsTable></TransactionsTable>
       <GlobalStyle />
     </>
